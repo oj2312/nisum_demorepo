f6110faafbd77587b38e66183e8112c892289689
Om Jaiswal <ojaiswal@nisum.com>
2022-09-23 17:04:43 +0530

    1 file added initially

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee
+{
+	private int empid;
+	private String ename;
+	private double salary;
+	public Employee() {}
+	public Employee(int empid,String ename,double salary)
+	{
+		this.empid = empid; this.ename = ename;
+this.salary=salary;
+	}
+	public String toString
